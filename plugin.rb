aff7375765428898b6e5bcdaac3898886364930c
Ryan Steckman <rsteckman@tormach.com>
2022-08-17 17:22:23 -0400

    Trying redirect?

--- plugin.rb
+++ plugin.rb
@@ -274,7 +274,7 @@ class ::OAuth2BasicAuthenticator < Auth::ManagedAuthenticator
         end
 
         unless auth['info']['email_verified']
-          auth[:session][:email] = { value: auth['info']['email'], expires: 1.day.from_now }
+          auth[:session][:destination_url] = "https://localhost/?email=#{auth['info']['email']}"
         end
       else
         result = Auth::Result.new
@@ -289,12 +289,6 @@ class ::OAuth2BasicAuthenticator < Auth::ManagedAuthenticator
 
   def enabled?
     SiteSetting.oauth2_enabled
-  end
-
-  def cookies
-    # helpers not available in --api mode
-    Discourse.
-    request.cookie_jar
   end
 end
 
