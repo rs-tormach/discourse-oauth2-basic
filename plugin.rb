2529e1b0092a8b94f904172d6667cdf5aa1361da
Ryan Steckman <rsteckman@tormach.com>
2022-08-17 15:52:03 -0400

    Fixing require.

--- plugin.rb
+++ plugin.rb
@@ -7,7 +7,7 @@
 # url: https://github.com/discourse/discourse-oauth2-basic
 # transpile_js: true
 
-include ActionDispatch::Cookies
+require 'middleware/cookies'
 enabled_site_setting :oauth2_enabled
 
 class ::OmniAuth::Strategies::Oauth2Basic < ::OmniAuth::Strategies::OAuth2
