7f148b2743c5954b9d1ad02383e1bb3c5bdebb07
Ryan Steckman <rsteckman@tormach.com>
2024-02-13 17:07:09 -0500

    Solving linting.

--- assets/javascripts/discourse/components/resend-confirm-login-modal.js
+++ assets/javascripts/discourse/components/resend-confirm-login-modal.js
@@ -1,12 +1,12 @@
 import Component from "@ember/component";
 import { action } from "@ember/object";
-//import discourseComputed from "discourse-common/utils/decorators";
+import discourseComputed from "discourse-common/utils/decorators";
 import { resendActivationEmail } from "discourse/lib/user-activation";
 import { wavingHandURL } from "discourse/lib/waving-hand-url";
 
 export default class ResendConfirmLoginModal extends Component.extend()
 {
-  //@discourseComputed("wavingHandURL")
+  @discourseComputed()
   wavingHandURL() {
     return wavingHandURL();
   }
@@ -16,44 +16,51 @@ export default class ResendConfirmLoginModal extends Component.extend()
     const vars = query.split("&");
     for (let i=0;i<vars.length;i++) {
             let pair = vars[i].split("=");
-            if(pair[0] == variable){return pair[1];}
+            if(pair[0] === variable){return pair[1];}
     }
     return(false);
   }
 
   init() {
-    console.log('init entry.');
+    super.init(...arguments);
+    console.log('init entry.'); //eslint-disable-line no-console
     const email = this.getQueryVariable('e');
     if (email) { this.set('email', window.atob(email)); }
     const username = this.getQueryVariable('u');
     if (username) { this.set('username', window.atob(username)); }
     if (this.get('email') && this.get('username')) {
       this.set('renderOk', true);
-      console.log('renderOk');
+      console.log('renderOk'); //eslint-disable-line no-console
       if (history.replaceState) {
         const cleanup = window.location.protocol + "//" + window.location.host + window.location.pathname;
         history.replaceState({path:cleanup},'',cleanup);
-        alert('REPLACED!');
+        alert('REPLACED!'); //eslint-disable-line no-alert
       }
     } else { this.set('renderOk', false); }
-    this._super();
   }
 
   didRender() {
+    super.didRender(...arguments);
     if (this.get('renderOk')) {
-      console.log('Hide login-modal')
-      $('.login-modal-body .login-left-side').addClass('hidden');
-      $('.login-modal-body .login-right-side').addClass('hidden');
-      $('#modal-alert').addClass('hidden');
+      console.log('Hide login-modal'); //eslint-disable-line no-console
+      const modalBodyLeft = document.querySelector('.login-modal-body .login-left-side');
+      modalBodyLeft.className = 'login-left-side hidden';
+      const modalBodyRight = document.querySelector('.login-modal-body .login-right-side');
+      modalBodyRight.className = 'login-right-side hidden';
+      const modalAlert = document.getElementById('modal-alert');
+      modalAlert.className += ' hidden';
     }
   }
 
   @action
   sendActivationEmail() {
-    $('.resend-activation-button').prop("disabled",true);
+    const resendButton = document.querySelector('.resend-activation-button');
+    resendButton.disabled = true;
     resendActivationEmail(this.get('username')).then(() => {
-      $('#not-activated').addClass('hidden');
-      $('#sent-activation').removeClass('hidden');
+      const notActivated = document.getElementById('not-activated');
+      notActivated.className += ' hidden';
+      const sentActivation = document.getElementById('sent-activation');
+      sentActivation.className = sentActivation.replace(' hidden', '');
     });
   }
 }
