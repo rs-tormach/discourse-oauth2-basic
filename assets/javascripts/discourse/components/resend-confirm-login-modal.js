b17f89572a258ccf76556093cf618ad226148c67
Ryan Steckman <rsteckman@tormach.com>
2024-02-13 16:41:22 -0500

    Shot in the dark - restructure component.

--- assets/javascripts/discourse/components/resend-confirm-login-modal.js
+++ assets/javascripts/discourse/components/resend-confirm-login-modal.js
@@ -1,38 +1,43 @@
 import Component from "@ember/component";
-import discourseComputed from "discourse-common/utils/decorators";
+import { action } from "@ember/object";
+//import discourseComputed from "discourse-common/utils/decorators";
 import { resendActivationEmail } from "discourse/lib/user-activation";
 import { wavingHandURL } from "discourse/lib/waving-hand-url";
 
-export default Component.extend({
-  @discourseComputed
-  wavingHandURL: () => wavingHandURL(),
+export default class ResendConfirmLoginModal extends Component.extend()
+{
+  //@discourseComputed("wavingHandURL")
+  wavingHandURL() {
+    return wavingHandURL();
+  }
 
-  getQueryVariable: function (variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i=0;i<vars.length;i++) {
-            var pair = vars[i].split("=");
+  getQueryVariable(variable) {
+    const query = window.location.search.substring(1);
+    const vars = query.split("&");
+    for (let i=0;i<vars.length;i++) {
+            let pair = vars[i].split("=");
             if(pair[0] == variable){return pair[1];}
     }
     return(false);
-  },
+  }
 
   init() {
     console.log('init entry.');
-    var email = this.getQueryVariable('e');
+    const email = this.getQueryVariable('e');
     if (email) { this.set('email', window.atob(email)); }
-    var username = this.getQueryVariable('u');
+    const username = this.getQueryVariable('u');
     if (username) { this.set('username', window.atob(username)); }
     if (this.get('email') && this.get('username')) {
       this.set('renderOk', true);
       console.log('renderOk');
       if (history.replaceState) {
-        var cleanup = window.location.protocol + "//" + window.location.host + window.location.pathname;
+        const cleanup = window.location.protocol + "//" + window.location.host + window.location.pathname;
         history.replaceState({path:cleanup},'',cleanup);
+        alert('REPLACED!');
       }
     } else { this.set('renderOk', false); }
     this._super();
-  },
+  }
 
   didRender() {
     if (this.get('renderOk')) {
@@ -41,15 +46,14 @@ export default Component.extend({
       $('.login-modal-body .login-right-side').addClass('hidden');
       $('#modal-alert').addClass('hidden');
     }
-  },
+  }
 
-  actions: {
-    sendActivationEmail() {
-      $('.resend-activation-button').prop("disabled",true);
-      resendActivationEmail(this.get('username')).then(() => {
-        $('#not-activated').addClass('hidden');
-        $('#sent-activation').removeClass('hidden');
-      });
-    },
-  },
-});
+  @action
+  sendActivationEmail() {
+    $('.resend-activation-button').prop("disabled",true);
+    resendActivationEmail(this.get('username')).then(() => {
+      $('#not-activated').addClass('hidden');
+      $('#sent-activation').removeClass('hidden');
+    });
+  }
+}
